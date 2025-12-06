8418499d621587b2de9bb93f8aa511d2b854452d
3FALESSIOGIUSTINIANI <matgiu1000@gmail.com>
2025-12-06 21:36:46 +0100

    esercizi

---
+++ esercizi_25-12-2/espag117num1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int num;
+printf("inserisci il numero da verificare se è negativo o positivo : ");
+scanf("%d", &num);
+if (num<0)
+printf("il numero %d è negativo.", num);
+else
+printf("il numero %d è positivo.", num);
+}
