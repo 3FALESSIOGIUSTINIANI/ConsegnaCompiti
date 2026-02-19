fb068f477fcaa104ee5a33f1b217cc506f4e80d9
3F2025GiustinianiA <matgiu1000@gmail.com>
2026-02-19 09:14:08 +0100

    elimino file

--- lab/26-02-12/bubblesort.cpp
+++ lab/26-02-12/bubblesort.cpp
@@ -34,8 +34,7 @@ for (int i = 0; i < n - 1; i++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+                arr[j + 1] = temp;            }
     }
 }
 }
