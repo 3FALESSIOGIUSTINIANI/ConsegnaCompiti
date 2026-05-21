0b4a093cb56e840f9eeae1687d506b7defc89bcc
3F2025GiustinianiA <matgiu1000@gmail.com>
2026-05-21 09:19:08 +0200

    esercizio

--- lab/26-05-21/classi.cpp
+++ lab/26-05-21/classi.cpp
@@ -1,54 +1,84 @@
 #include <iostream>
 
 using namespace std;
+
 class Veicolo {
 public:
-
     virtual ~Veicolo() = default;
 
-    void identifica() const {
-        cout << "[Veicolo - Non Virtuale] Sono un mezzo di trasporto generico." << endl;
-
-        virtual void identifica();
-        virtual void muoviti();
-        virtual void suonaClacson();
+    void eseguiDiagnostica() const {
+        cout << " INIZIO DIAGNOSTICA VEICOLO " << endl;
+        
+        controlloElettronica(); 
+        controlloMotore();       
+        controlloFreni();        
+        
+        cout << " FINE DIAGNOSTICA " << endl << endl;
     }
-   
-};
 
+protected:
+
+    virtual void controlloElettronica() const = 0;
+    virtual void controlloMotore() const = 0;
+    virtual void controlloFreni() const = 0;
+};
 
 class Automobile : public Veicolo {
-public:
-
-
-     virtual void muoviti() const {
-        cout << "[Veicolo - Virtuale 1] Mi muovo in qualche modo." << endl;
+protected:
+    void controlloElettronica() const override { 
+        cout << "[Auto] Controllo centralina di bordo: OK." << endl; 
     }
-
-    virtual void suonaClacson() const {
-        cout << "[Veicolo - Virtuale 2] Emetto un suono generico." << endl;
+    void controlloMotore() const override { 
+        cout << "[Auto] Controllo livello olio e liquido radiatore: OK." << endl; 
     }
-
-    virtual int getNumeroRuote() const {
-        return 0;
+    void controlloFreni() const override { 
+        cout << "[Auto] Verifica usura pastiglie freni anteriori: OK." << endl; 
     }
 };
 
 
+class Moto : public Veicolo {
+protected:
+    void controlloElettronica() const override { 
+        cout << "[Moto] Controllo fari e indicatori di direzione: OK." << endl; 
+    }
+    void controlloMotore() const override { 
+        cout << "[Moto] Controllo tensione e lubrificazione catena: OK." << endl; 
+    }
+    void controlloFreni() const override { 
+        cout << "[Moto] Controllo pressione circuito freno anteriore: OK." << endl; 
+    }
+};
+
+class Camion : public Veicolo {
+protected:
+    void controlloElettronica() const override { 
+        cout << "[Camion] Controllo del cronotachigrafo digitale: OK." << endl; 
+    }
+    void controlloMotore() const override { 
+        cout << "[Camion] Controllo dei livelli di AdBlue e pressione turbo: OK." << endl; 
+    }
+    void controlloFreni() const override { 
+        cout << "[Camion] Verifica del sistema frenante pneumatico ad aria: OK." << endl; 
+    }
+};
+
 int main() {
-    cout << "Creazione di un oggetto Automobile gestito da un puntatore Veicolo..." << endl;
 
-    Veicolo* mioVeicolo = new Automobile();
-
-    mioVeicolo->identifica();
-    
-    cout << endl;
+    Veicolo* flotta[3];
+    flotta[0] = new Automobile();
+    flotta[1] = new Moto();
+    flotta[2] = new Camion();
 
 
-    mioVeicolo->muoviti();
-    mioVeicolo->suonaClacson();
-    cout << "[Automobile - Virtuale 3] Numero di ruote: " << mioVeicolo->getNumeroRuote() << endl;
+    for (int i = 0; i < 3; i++) {
+        flotta[i]->eseguiDiagnostica();
+    }
 
-    delete mioVeicolo;
 
+    for (int i = 0; i < 3; i++) {
+        delete flotta[i];
+    }
+
+    return 0;
 }
