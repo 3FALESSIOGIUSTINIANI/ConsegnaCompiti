f991af740860f5e4e3da03c4b01459a560282cba
3F2025GIUSTINIANIA <matgiu1000@gmail.com>
2026-05-24 10:59:11 +0200

    step 50

--- lab/agenda/main.cpp
+++ lab/agenda/main.cpp
@@ -3,25 +3,31 @@
 using namespace std;
 
 int main() {
+    GestoreContatti gestore;
     int scelta;
-    inizializza_contatti();
 
     do {
-        cout << "\n====== MENU RUBRICA ======\n";
-        cout << "1. Ricerca contatto (per cognome)\n";
-        cout << "2. Inserisci nuovo contatto\n";
-        cout << "0. Esci (i dati andranno persi)\n";
+        cout << "\n====== MENU RUBRICA (CRUD) ======\n";
+        cout << "1. CREATE  - Inserisci nuovo contatto\n";
+        cout << "2. SEARCH  - Ricerca contatto per cognome\n";
+        cout << "3. READ    - Visualizza tutti i contatti\n";
+        cout << "4. UPDATE  - Modifica contatto\n";
+        cout << "5. DELETE  - Elimina contatto\n";
+        cout << "0. Esci\n";
         cout << "Scelta: ";
         cin >> scelta;
 
         switch (scelta) {
-            case 1: ricerca_binaria();   break;
-            case 2: aggiungi_contatto(); break;
+            case 1: gestore.aggiungi_contatto();  break;
+            case 2: gestore.ricerca_binaria();    break;
+            case 3: gestore.visualizza_tutti();   break;
+            case 4: gestore.modifica_contatto();  break;
+            case 5: gestore.elimina_contatto();   break;
             case 0:
-                cout << "Uscita dal programma. La memoria RAM verra' liberata." << endl;
+                cout << "Uscita dal programma." << endl;
                 break;
             default:
-                cout << "Scelta non valida, riprova." << endl;
+                cout << "Scelta non valida!" << endl;
         }
     } while (scelta != 0);
 
